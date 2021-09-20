207b424cb8a8530d7304c609d8ba9a27c481cd0d
Jared Godar <jared.godar@gmail.com>
2021-09-20 12:01:25 -0500

    Add limit_exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+-- 2. First ten unique last names in descneding order
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+-- 3. Hired 90s & Born Christmas. First 5 sorted by hire date Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup
+
+SELECT emp_no,
+       first_name,
+       last_name,
+       birth_date,
+       hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date LIKE '%-12-25'
+ORDER BY hire_date
+LIMIT 5;
+
+-- 4. Find 10th 'page' of results
+
+SELECT emp_no,
+       first_name,
+       last_name,
+       birth_date,
+       hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date LIKE '%-12-25'
+ORDER BY hire_date
+LIMIT 5
+OFFSET 45;
