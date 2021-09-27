2eabd888b3d3eeaded0c62f275e041f9bf37d229
Jared Godar <jared.godar@gmail.com>
2021-09-27 11:53:09 -0500

    Completed subqueries exercises (no bonuses)

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -32,11 +32,14 @@ WHERE first_name = 'Aamod';
 SELECT title, last_name, first_name
 FROM titles
 JOIN employees USING(emp_no)
-WHERE emp_no IN(
+JOIN dept_emp USING(emp_no)
+WHERE dept_emp.to_date > now()
+AND
+ emp_no IN(
 	SELECT emp_no
 	FROM employees
-	WHERE first_name = 'Aamod')
-AND to_date > now();
+	WHERE first_name = 'Aamod'
+    );
 
 -- 3. How many people in the employees table are no longer working for the company? 85,108
 
@@ -86,7 +89,39 @@ JOIN employees using(emp_no)
     WHERE to_date > now()
 );
 
--- 6. How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+-- 6. How many current salaries are within 1 standard deviation of the current highest salary? 211
+# (Hint: you can use a built in function to calculate the standard deviation.) 
+# What percentage of all salaries is this? = (211/240124)*100 = 0.0879 % 
+
+#S1 current highest 158,220
+
+SELECT max(salary)
+FROM salaries
+WHERE to_date > now() as max_salary;
+
+## Deviation 16,904
+select stddev(salary) from salaries;
+
+#S Z score
+SELECT salary, 
+    (salary - (SELECT AVG(salary) FROM salaries)) 
+    / 
+    (SELECT stddev(salary) FROM salaries) AS zscore
+FROM salaries;
+
+SELECT emp_no, salary
+FROM salaries
+WHERE salary > ((SELECT max(salary)
+FROM salaries
+WHERE to_date > now()) - (SELECT stddev(salary) from salaries));
+
+select emp_no, salary
+from salaries
+where to_date > now();
+
+
+
+(
 
 /*
 Hint Number 1 You will likely use a combination of different kinds of subqueries.
