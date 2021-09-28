f0fb1c3c83ac783cb9ac122e1096b1a453f40ac5
Jared Godar <jared.godar@gmail.com>
2021-09-28 14:55:11 -0500

    Finalize temporary table exercises, add case exercises

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -14,15 +14,15 @@ USE hopper_1545;
 SELECT * FROM employees_with_departments;
 
 
-/*2. Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+/*Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
 Update the table so that full name column contains the correct data
 Remove the first_name and last_name columns from the table.
 What is another way you could have ended up with this same table?
 */
 
 select database();
-
-ALTER TABLE employees_with_departments ADD full_name VARCHAR(100);
+describe employees.employees;
+ALTER TABLE employees_with_departments ADD full_name VARCHAR(31);
 UPDATE employees_with_departments
 SET full_name = CONCAT(first_name, ' ', last_name);
 ALTER TABLE employees_with_departments
@@ -32,7 +32,22 @@ DROP COLUMN last_name;
 
 SELECT * FROM employees_with_departments;
 
-#3. Create a temporary table based on the payment table from the sakila database. Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
+# Simpler way
+
+## To programatically drop a table instead of disconnecting and reconnecting to clear temporary tables
+	# DROP TABLE employees_with_departments;
+	# Show tables
+    
+create temporary table hopper_1545.employees_with_departments AS (
+	select concat(first_name, ' ', last_name) as full_name,  dept_name
+	from employees
+	join dept_emp using(emp_no)
+	join departments using (dept_no)
+	where dept_emp.to_date > curdate()
+);
+
+
+#2. Create a temporary table based on the payment table from the sakila database. Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
 
 USE sakila;
 
@@ -51,13 +66,14 @@ SET amount_cents = (amount * 100);
 
 USE employees;
 
+/*
 create temporary table hopper_1545.department_pay AS (
 	select emp_no, first_name, last_name, dept_name, salary
 	from employees
 	join dept_emp using(emp_no)
 	join departments using (dept_no)
 	join salaries using(emp_no)
-);
+);*/
 
 create temporary table hopper_1545.department_avg_pay AS (
 	SELECT avg(salary), dept_name from salaries
@@ -75,10 +91,13 @@ ALTER TABLE department_avg_pay ADD avg_salary float;
 ALTER TABLE department_avg_pay ADD std_salary float;
 
 UPDATE department_avg_pay
-SET avg_salary = 63805.4;
+SET avg_salary = 63805.4; -- 63811 on ryan's
+
+#SET avg_salary = (select avg(salary) from employees.salaries);
 
 UPDATE department_avg_pay
-SET std_salary = 16900.85;
+SET std_salary = 16900.85; -- 16905 on ryan's
+# set std_salary = (select stddev(salary) from employees.salaries);
 
 UPDATE department_avg_pay
 SET z_score = (`avg(salary)` - avg_salary) 
